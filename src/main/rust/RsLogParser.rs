cca87bd555100713548020901b0291f5ed3c0488
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2020-10-31 16:38:14 +0200

    Merge into master

--- src/main/rust/RsLogParser.rs
+++
@@ -1,9 +0,0 @@
-use coroutine::asymmetric::*;
-
-let mut coro = Coroutine::spawn(|coro, val| {
-    println!("Inside {}", val);
-    coro.yield_with(val + 1)
-});
-
-println!("Resume1 {}", coro.resume(0).unwrap());
-println!("Resume2 {}", coro.resume(2).unwrap());
